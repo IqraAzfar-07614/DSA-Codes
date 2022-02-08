11845b8b91717a5271c516476db28225b9b94c50
Ali Muhammad Asad <93670153+AliMuhammadAsad@users.noreply.github.com>
2022-02-08 11:16:39 +0500

    Update HW1.py

--- HW1.py
+++ HW1.py
@@ -1,3 +1,31 @@
+Q1) Paranthetical - Checks if a given sequence of brackets can be corrected by moving at most 1 bracket from its position. Returns Yes if it can else returns no
+#Move at most 1 bracket from its place
+#Reversing any bracket is not allowed
+#Only round brackets => '(' or ')'
+
+seq = input()
+
+def isCorrect(seq):
+    if len(seq) % 2 == 1: #if odd sequence, then can't be corrected so returns No 
+        return "No"
+    else:
+        count = 0 #counter to check the times closing and opening brackets occur for comparison
+        for x in seq:
+            if x == '(':
+                count += 1 #count +1 on each opening bracket
+            elif x == ')':
+                count -= 1 #count -1 on each closing bracket
+                if count == -2: #if count = -2, then 2 brackets at the wrong place, thus can't be corrected and breaks to return No
+                    break
+        if count == 0: #Correct as count = 0, so returns Yes
+            return "Yes"
+        else: 
+            return "No" #returns No for all other values of count as then it can't be corrected if the count is too high even 
+
+print(isCorrect(seq))
+
+----------------------------------------------------------------------------------------------------------------------------------------------------
+
 Q2) Balanced Braces - Checks if the given brackets are balanced in the sequence or not - returns False if not balanced, returns True if balanced
 #Uses only 1 stack and stack ADT operations
 #Counters of any sort are not allowed to count the brackets
