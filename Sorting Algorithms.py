d8197d0f2daac69249eb27b7e9c005d51b1f8594
Ali Muhammad Asad <93670153+AliMuhammadAsad@users.noreply.github.com>
2022-02-21 10:47:33 +0500

    Update Sorting Algorithms.py

--- Sorting Algorithms.py
+++ Sorting Algorithms.py
@@ -50,3 +50,40 @@ def insertion_sort(A):
     return A
 
 print(insertion_sort(A))
+
+
+-------------------------------------------------------------------------------------------------
+
+Merge Sort
+#Merge sort is a divide and conquer algorithm
+#It divides the problem into two halves, and calls itself for the two halves, and it is repeated using recursion 
+#Then when the division is done and the parts are sorted, they are then merged.
+#Complexity is O(n*log(n))
+
+def merge_sort(lst):
+    if len(lst) > 1:
+        mid = len(lst) // 2 #mid of the list
+        left, right = lst[:mid], lst[mid:] #dividing the list into two halves
+        merge_sort(right), merge_sort(left) #sorting the two halves
+        i = j = k = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                lst[k] = left[i]
+                i += 1
+            else:
+                lst[k] = right[j]
+                j += 1
+            k += 1
+        while i < len(left):
+            lst[k] = left[i]
+            i += 1
+            k += 1
+        while j < len(right):
+            lst[k] = right[j]
+            j += 1
+            k += 1
+    return lst
+
+arr = [12, 11, 13, 5, 6, 7]
+
+print(merge_sort(arr))
